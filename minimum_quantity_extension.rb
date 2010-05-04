6dc8d34c8bda49a194004e6167054bba0b0d711b
Peter Berkenbosch <peter@pero-ict.nl>
2010-05-04 22:01:57 +0200

    only set minimum on product.

--- minimum_quantity_extension.rb
+++ minimum_quantity_extension.rb
@@ -20,7 +20,7 @@ class MinimumQuantityExtension < Spree::Extension
 			
 			private
 			def must_meet_minimum
-				minimum = variant.is_master? ? variant.product.minimum_quantity : variant.minimum_quantity
+				minimum = variant.product.minimum_quantity
 				minimum ||= 0
 				errors.add(:quantity, "does not meet minimum quantity required") if quantity < minimum && quantity != 0
 			end
